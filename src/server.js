ffe0025fb5504f4a573f96f7b61eaec67826ff08
Edvaldo de Fendi <edvaldo.defendi@gmail.com>
2022-08-26 16:39:57 -0300

    Server

--- src/server.js
+++ src/server.js
@@ -8,7 +8,9 @@ server.set('view engine', 'ejs')
 
 server.use(express.static("public"))
 
-server.set ('views', path.join(__dirname, 'views'))
+server.set('views', path.join(__dirname, 'views'))
+
+server.use(express.urlencoded({ extended: true }))
 
 server.use(route)
 
